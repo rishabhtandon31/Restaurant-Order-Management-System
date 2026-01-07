14f2a10402dfc5c5fa6655741256c3a3a0f5b00e
Rishabh <rishabh.tandon@paytm.com>
2026-01-07 18:29:11 +0530

    excpetion related changes

--- src/main/java/com/jumio/roms/exception/PaymentException.java
+++ src/main/java/com/jumio/roms/exception/PaymentException.java
@@ -1,5 +1,23 @@
 package com.jumio.roms.exception;
 
 public class PaymentException extends RuntimeException {
-    public PaymentException(String message) { super(message); }
+    private final ErrorCode errorCode;
+
+    public PaymentException(String message) {
+        this(ErrorCode.PAYMENT_ERROR, message);
+    }
+
+    public PaymentException(ErrorCode code, String message) {
+        super(message);
+        this.errorCode = code;
+    }
+
+    public PaymentException(ErrorCode code, String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = code;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
 }
